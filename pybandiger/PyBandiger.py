0dbf444973a32aca35b247a6d5f16e92eea59d64
LansariFedi <lansarifedi7@gmail.com>
2024-11-19 21:29:58 +0100

    Commit

--- pybandiger/PyBandiger.py
+++ pybandiger/PyBandiger.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+import pandas as pd
 
 class PyBandiger:
     def __init__(self):
@@ -6,6 +7,7 @@ class PyBandiger:
         self.ss = StandardScaler()
 
     def clean(self, data):
+        data = data.copy()
         cat_col = data.select_dtypes(include='object').columns
         num_col = data.select_dtypes(include=['int', 'float']).columns
         for col in cat_col:
@@ -15,6 +17,7 @@ class PyBandiger:
         return data
 
     def EncodeAndScale_fit(self, data):
+        data = data.copy()
         cat_col = data.select_dtypes(include='object').columns
         num_col = data.select_dtypes(include=['int', 'float']).columns
         for col in cat_col:
@@ -25,9 +28,13 @@ class PyBandiger:
         return data
 
     def EncodeAndScale_transform(self, data):
+        data = data.copy()
         cat_col = data.select_dtypes(include='object').columns
         num_col = data.select_dtypes(include=['int', 'float']).columns
         for col in cat_col:
-            data[col] = self.le[col].transform(data[col])
+            if col in self.le:
+                data[col] = self.le[col].transform(data[col])
+            else:
+                raise ValueError(f"Column '{col}' was not fitted with an encoder.")
         data[num_col] = self.ss.transform(data[num_col])
         return data
